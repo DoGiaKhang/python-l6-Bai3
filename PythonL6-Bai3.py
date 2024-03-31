ca25379b2d94b66cf02e2374f91c9c8f9143daaa
DoGiaKhang <164725044+DoGiaKhang@users.noreply.github.com>
2024-03-31 15:28:03 +0700

    python l6 bai3

---
+++ PythonL6-Bai3.py
@@ -0,0 +1,45 @@
+#Bai 3
+i = 0
+print("Cau 1.Python duoc phat hanh vao nam nao?")
+print("A.1991")
+print("B.1989")
+print("C.2000")
+print("D.1995")
+ans = input("Nhap dap an (A, B, C, D): ")
+if ans == "A":
+    print("dap an dung")
+    i+=1
+else:
+    print("dap an sai")
+    print("dap an dung la A")
+
+print()
+print("Cau 2.Ai la nguoi tao ra Python?")
+print("A.Guido van Rossum")
+print("B.Dennis Ritchie")
+print("C.James Gosling")
+print("D.Bjame Stroustrup")
+ans = input("Nhap dap an (A, B, C, D): ")
+if ans == "A":
+    print("dap an dung")
+    i+=1
+else:
+    print("dap an sai")
+    print("dap an dung la A")
+
+print()
+print("Cau 3.Cau lenh nao duoi day dung de hien thi du lieu ra man hinh trong Python?")
+print("A.print()")
+print("B.cout<<")
+print("C.System out printIn()")
+print("D.echo")
+ans = input("Nhap dap an (A, B, C, D): ")
+if ans == "A":
+    print("dap an dung")
+    i+=1
+else:
+    print("dap an sai")
+    print("dap an dung la A")
+print()
+print("so diem cua ban la:",i)
+
